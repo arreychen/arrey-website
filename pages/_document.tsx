364548c8e4bb47b7a6fde04567ccff4f61ca9d4e
ArreyChan <63032433+ArreyChan@users.noreply.github.com>
2022-08-09 22:48:04 +0800

    feat: add a loader during the first loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ import Document, {
   NextScript,
   DocumentInitialProps,
 } from 'next/document';
-
+import load from '../components/css/load';
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
@@ -23,13 +23,24 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@700&family=Bangers&family=Hammersmith+One&family=ZCOOL+XiaoWei&display=swap'
             rel='stylesheet'
           />
+          <style>
+            {load}
+          </style>
         </Head>
+
         <body >
+          <div className='globalLoader'>
+            <div className="loader">
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+
           <Main />
           <NextScript />
         </body>
-
       </Html>
+
     );
   }
 }
